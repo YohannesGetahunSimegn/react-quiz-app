6fc82c89f8b899d349dfa67d5c98e5afe0c52dff
Yohannes G.Simegne <ygetahun37@gmail.com>
2025-03-31 10:15:43 +0300

    json web server and data prepared

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-import DateCounter from "./DateCounter";
+// import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
+
+// const initialState = {
+// questions: [],
+// };
+
+// function reducer(state, action) {}
+
 function App() {
+  // const [state, dispatch] = useReducer(reducer, initialState);
+
+  // useEffect(function () {
+  //   fetch("http://localhost:9000/questions")
+  //     .then((res) => res.json())
+  //     .then((data) => console.log(data))
+  //     .catch((err) => console.error("Error"));
+  // });
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+
+      <Main>
+        <p>1/15</p>
+        <p>Questions</p>
+      </Main>
     </div>
   );
 }
